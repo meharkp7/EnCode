c40683f7c21647307f19be0141cd582f8a33f82f
meharkp7 <meharkapoor7@gmail.com>
2025-01-10 20:50:45 +0530

    Add files via upload

---
+++ tts.js
@@ -0,0 +1,33 @@
+const textInput = document.getElementById('text-input');
+const convertBtn = document.getElementById('convert-btn');
+const stopBtn = document.getElementById('stop-btn');
+
+let speechSynthesis = window.speechSynthesis;
+let speechSynthesisUtterance = new SpeechSynthesisUtterance();
+
+function setIndianEnglishVoice() {
+    let voices = speechSynthesis.getVoices();
+    let indianVoice = voices.find(voice => voice.lang.includes('en-IN'));
+    speechSynthesisUtterance.voice = indianVoice || voices[0];  
+}
+
+speechSynthesis.onvoiceschanged = setIndianEnglishVoice;
+
+convertBtn.addEventListener('click', () => {
+    let text = textInput.value.trim();
+    if (text !== '') {
+        speechSynthesisUtterance.text = text;
+        speechSynthesisUtterance.pitch = 1.0;
+        speechSynthesisUtterance.rate = 1.0;
+        speechSynthesisUtterance.volume = 1.0;
+        speechSynthesis.speak(speechSynthesisUtterance);
+        convertBtn.disabled = true;
+        stopBtn.disabled = false;
+    }
+});
+
+stopBtn.addEventListener('click', () => {
+    speechSynthesis.cancel();
+    convertBtn.disabled = false;
+    stopBtn.disabled = true;
+});
